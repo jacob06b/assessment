556eed1a6b63dac9fe62ee599023313921d573b6
Jacob Bramwell <jacob.bramwell@icloud.com>
2022-10-13 15:51:57 +0100

    assessment

---
+++ assessmentCode/assessmentCode/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+Console.WriteLine("11010010");
+//Console.WriteLine("Enter a whole number: " );
+////string Number1 = Console.ReadLine();
+//Console.WriteLine("Enter another whole number: ");
+//    string number2 = Console.ReadLine();
+//int temp1 = int.Parse(Number1);
+//int temp2 = int.Parse(number2);
+//while (temp1 != temp2)
+//    {
+//        if(temp1>temp2)
+//        {
+//            temp1 = temp1 - temp2;
+//        }
+//        else
+//        {
+//            temp1 = temp2 - temp1;
+
+//        }
+//    break;
+//    }
+//int result = temp1;
+//Console.WriteLine($"{result} is GCF of {Number1} and {number2}");
+//return result;
+
+//// while loop was written with temp1 and temp2 instead of number1 and 2 because number1 and number2 are in string format so the '>' operand cannot be applied to them        
+//Console.WriteLine(Question6.division(210));
+//public class Question6
+//{
+//    public static string division(int num1)
+//    {
+
+//        int total = 0;
+//        int counter = 0;
+//        while (num1 != 0)
+//        {
+//            if (num1 % 2 == 0)
+//            {
+//                Console.WriteLine("0");
+//                num1 = num1 / 2;
+//            }
+//            else
+//            {
+//                num1 = num1 - 1;
+//                Console.WriteLine("1");
+//                num1 = num1 / 2;
+//            }
+            
+//            //string output = Console.ReadLine();
+//            if (output == "1")
+//            {
+//                int power = 2^counter;
+//                total = total + power;
+//            }
+//            counter++;
+//            string BinNum = Convert.ToString(total, 2);
+//            Console.WriteLine(BinNum);
+//            return BinNum;
+
+//        }
+//    }
+//}
+
